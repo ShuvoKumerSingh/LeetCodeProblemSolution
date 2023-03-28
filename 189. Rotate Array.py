96e5793b319e2bbba089980dc52b88b686259c86
ShuvoKumerSingh <87059001+ShuvoKumerSingh@users.noreply.github.com>
2023-03-28 23:14:22 +0600

    189. Rotate Array

---
+++ 189. Rotate Array.py
@@ -0,0 +1,56 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        
+        l,r=0,len(nums)-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l,r=l+1,r-1
+        
+        l,r=0,k-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l,r=l+1,r-1
+        
+        l,r=k,len(nums)-1
+        while l<r:
+            nums[l],nums[r]=nums[r],nums[l]
+            l,r=l+1,r-1
+'''
+Another Solution is
+"'
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        lenth=len(nums)
+        l,r=0,lenth-1
+        
+        if lenth==k:
+            return nums
+        elif lenth<k:
+             while k-lenth>0:
+                if nums[l]<nums[r]:
+                    nums.insert(0,nums[r])
+                    nums.pop(r+1)
+                    r=r
+                    l=l+1
+                k-=1
+        else:
+            while k>0:
+                if nums[l]<nums[r]:
+                    nums.insert(0,nums[r])
+                    nums.pop(r+1)
+                    r=r
+                    l=l+1
+                    k-=1
+                else:
+                    nums.insert(0,nums[r])
+                    nums.pop(r+1)
+                    r=r
+                    l=l+1
+                    k-=1

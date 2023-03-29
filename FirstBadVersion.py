f2f2fe886570f9e55cdcad2b201797575befc02d
ShuvoKumerSingh <87059001+ShuvoKumerSingh@users.noreply.github.com>
2023-03-30 00:14:34 +0600

    278. First Bad Version
    https://leetcode.com/problems/reverse-string/?envType=study-plan&id=algorithm-i

--- FirstBadVersion.py
+++ FirstBadVersion.py
@@ -2,7 +2,7 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         left = 1
         right = n
-        result = n
+        first_bad_version = n
         while left<=right:
             mid = left + (right-left)//2
             if isBadVersion(mid):
@@ -10,4 +10,4 @@ class Solution:
                 right = mid-1
             else:
                 left = mid+1
-        return result
+        return first_bad_version
